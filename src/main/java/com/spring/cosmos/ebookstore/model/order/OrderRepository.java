a65885b9229071d46c4729b3d290e21b62712156
Theo van Kraay <thvankra@microsoft.com>
2022-07-18 17:10:06 +0100

    add MIT license

--- src/main/java/com/spring/cosmos/ebookstore/model/order/OrderRepository.java
+++ src/main/java/com/spring/cosmos/ebookstore/model/order/OrderRepository.java
@@ -1,3 +1,5 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
 package com.spring.cosmos.ebookstore.model.order;
 
 import com.azure.spring.data.cosmos.repository.CosmosRepository;
